3d245e6c9bbc17666760cf771b1b0aba18b60e16
Nam-203 <cuciuiu357@gmail.com>
2024-05-08 16:29:37 +0700

    upload regiter and send and verified otp mail

--- controller/auth.js
+++ controller/auth.js
@@ -1,45 +1,127 @@
+const { response } = require("express");
 const User = require("../models/UserModel");
-
-exports.login = catchAsync(async (req, res, next) => {
-    const { email, password } = req.body;
-  
-    // console.log(email, password);
-  
-    if (!email || !password) {
-      res.status(400).json({
+const filterObj = require("../utils/filterObj");
+const otpGenerator = require("otp-generator");
+const signToken = (userId) => jwt.sign({ userId }, process.env.JWT_SECRET);
+exports.register = async (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body;
+  const filteredBody = filterObj(
+    req.body,
+    "firstName",
+    "lastName",
+    "password",
+    "email"
+  );
+  try {
+    const existing_user = await User.findOne({ email: email });
+    if (existing_user && !existing_user.verified) {
+      return res.status(400).json({
         status: "error",
-        message: "Both email and password are required",
+        message: "email already in use, please login again",
       });
-      return;
-    }
-  
-    const user = await User.findOne({ email: email }).select("+password");
-  
-    if (!user || !user.password) {
-      res.status(400).json({
-        status: "error",
-        message: "Incorrect password",
+    } else if (existing_user) {
+      await User.findByIdAndUpdate({ email: email }, filterObj, {
+        new: true,
+        validateModifiedOnly: true,
       });
-  
-      return;
+      //send otp mail to usser
+      req.userId = existing_user._id;
+      next();
+    } else {
+      //iff usser record iss not valiable here
+      const new_user = await User.create(filteredBody);
+      req.userId = new_user._id;
+      next();
     }
-  
-    if (!user || !(await user.correctPassword(password, user.password))) {
-      res.status(400).json({
-        status: "error",
-        message: "Email or password is incorrect",
-      });
-  
-      return;
-    }
-  
-    const token = signToken(user._id);
-  
-    res.status(200).json({
-      status: "success",
-      message: "Logged in successfully!",
-      token,
-      user_id: user._id,
-    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+exports.sendOTP = async (req, res, next) => {
+  const { userId } = req;
+  const new_otp = otpGenerator.generate(6, {
+    upperCaseAlphabets: false,
+    lowerCaseAlphabets: false, 
+    specialChars: false,
   });
+  const otp_expiry_time = Date.now()+ 10*60*1000;//10 minutes
+  await User.findByIdAndUpdate(userId, {
+    otp :new_otp,
+    otp_expiry_time
+  })
+  //todo send otp
+  return res.status(200).json({
+    status : "success",
+    message : "OTP sent successfully",
   
+  })  
+};
+exports.verifyOTP = async (req, res) => {
+  //verify and up usser 
+  const {email,otp} = req.body
+  const user = await User.findOne({email , otp_expiry_time :{$gt :Date.now()}})
+  if (!user) {
+    return res.status(400).json({
+      status : "error",
+      message : "Email invalid or OTP expired"
+    })
+  }
+  if (!await user.correctOTP(otp, user.otp)) {
+      return res.status(400).json({
+        status : "error",
+        message : "Incorrect OTP"
+      })
+
+  }
+  user.otp = undefined,
+  user.verified = true
+  await user.save({new:true,validateModifiedOnly:true})
+  const token = signToken(user._id)
+  return res.status(200).json({
+    status: "success",
+    message: "otp verified success!",
+    token,
+  });
+}
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // console.log(email, password);
+
+  if (!email || !password) {
+    res.status(400).json({
+      status: "error",
+      message: "Both email and password are required",
+    });
+    return;
+  }
+
+  const user = await User.findOne({ email: email }).select("+password");
+
+  if (!user || !user.password) {
+    res.status(400).json({
+      status: "error",
+      message: "Incorrect password",
+    });
+
+    return;
+  }
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    res.status(400).json({
+      status: "error",
+      message: "Email or password is incorrect",
+    });
+
+    return;
+  }
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    message: "Logged in successfully!",
+    token,
+    user_id: user._id,
+  });
+});
--- models/UserModel.js
+++ models/UserModel.js
@@ -1,31 +1,49 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  fistName: { type: string, require: true },
-  lastName: { type: string, require: true },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: function (email) {
-        return String(email)
-          .toLowerCase()
-          .match(
-            ` /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/`
-          );
+const userSchema = new mongoose.Schema(
+  {
+    fistName: { type: string, require: true },
+    lastName: { type: string, require: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (email) {
+          return String(email)
+            .toLowerCase()
+            .match(
+              ` /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/`
+            );
+        },
       },
     },
+    password: { type: String, required: true },
+    passwordChangeAt: { type: Date },
+    passwordResetToken: { type: String },
+    passwordExpireIn: { type: Date },
+    avatar: { type: String },
+    verified: { type: Boolean, default: false },
+    otp: { type: Number },
+    otp_expiry_time: { type: Date },
   },
-  password: { type: String, required: true },
-  passwordChangeAt: { type: Date },
-  passwordResetToken: { type: String },
-  passwordExpireIn: { type: Date },
+  {
+    timestamps: true,
+  }
+);
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("otp") || !this.otp) return next();
 
-  avatar: { type: String },
-},
-{
-    timestamps: true
+  // Hash the otp with cost of 12
+  this.otp = await bcrypt.hash(this.otp.toString(), 12);
+
+  console.log(this.otp.toString(), "FROM PRE SAVE HOOK");
+
+  next();
 });
+userSchema.methods.correctOTP = async function (candidateOTP, userOTP) {
+  return await bcrypt.compare(candidateOTP, userOTP);
+};
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
-
